70ad0dd0649894392089be183c924593f2d43e63
jok3r <juan.herrera@element.com.mx>
2018-04-19 16:45:41 -0500

    debug

--- index.js
+++ index.js
@@ -30,13 +30,8 @@ app.post('/webhook', (req, res) => {
 						handlePostback(sender_psid, webhook_event.postback);
 					}
 				}else{
-					console.log('--DEBUG----DEBUG----DEBUG--');
-					console.log(event);
-					console.log(event.postback);
-					console.log(event.postback.payload);
-					console.log('--DEBUG----DEBUG----DEBUG--');
 
-					if(event.postback && event.postback.payload === USER_DEFINED_PAYLOAD ){
+					if(event.postback && event.postback.payload === GET_STARTED_PAYLOAD ){
 						var msg = "El Mundial ya está aquí y todos queremos ser parte de él. Apoya a tu equipo favorito en nuestro Mundial DeBolsillo. ¡Participa!";
 						callSendAPI(sender_psid, webhook_event.message);        
 					}
