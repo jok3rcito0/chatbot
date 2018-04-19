0c5d2c7c7560f49c16f117120226036af57d49e3
jok3r <juan.herrera@element.com.mx>
2018-04-19 15:15:01 -0500

    return simple response

--- index.js
+++ index.js
@@ -8,30 +8,43 @@ const
 app.listen(process.env.PORT || 443, () => console.log('webhook is listening'));
 
 app.post('/webhook', (req, res) => { 
-  let body = req.body;
+	let body = req.body;
 
-  if (body.object === 'page') {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-      // Gets the message. entry.messaging is an array, but will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      //console.log(webhook_event);
-      //
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-    });
+	if (body.object === 'page') {
+		body.entry.forEach(function(entry) {
+			// Gets the message.
+			let webhook_event = entry.messaging[0];
+			//console.log(webhook_event);
 
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    res.sendStatus(404);
-  }
+			let sender_psid = webhook_event.sender.id;
+			//console.log('Sender PSID: ' + sender_psid);ç
+
+			if (webhook_event.message) {
+				handleMessage(sender_psid, webhook_event.message);        
+			} else if (webhook_event.postback) {
+				handlePostback(sender_psid, webhook_event.postback);
+			}
+		});
+
+		res.status(200).send('EVENT_RECEIVED');
+	} else {
+		res.sendStatus(404);
+	}
 
 });
 
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+	let response;
 
+	if (received_message.text) {    
+		response = {
+			"text": `You sent the message: "${received_message.text}". Now send me an image!`
+		}
+	}
+
+	callSendAPI(sender_psid, response);    
 }
 
 // Handles messaging_postbacks events
@@ -41,21 +54,26 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+	let request_body = {
+		"recipient": {
+			"id": sender_psid
+		},
+		"message": response
+	}
 }
 
 
 app.get('/webhook', (req, res) => {
-  let VERIFY_TOKEN = "Holamund0_$MwWi1t00os"
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-  if (mode && token) {
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      //console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);      
-    }
-  }
+	let VERIFY_TOKEN = "Holamund0_$MwWi1t00os"
+	let mode = req.query['hub.mode'];
+	let token = req.query['hub.verify_token'];
+	let challenge = req.query['hub.challenge'];
+	if (mode && token) {
+		if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+			//console.log('WEBHOOK_VERIFIED');
+			res.status(200).send(challenge);
+		} else {
+			res.sendStatus(403);      
+		}
+	}
 });
